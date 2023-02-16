15cbe041d32e8137d81fdbf53c77289ec482b120
JoaoPaulo-creator <joaosarto@gmail.com>
2023-02-16 20:54:13 -0300

    map

---
+++ hof/map.js
@@ -0,0 +1,21 @@
+const cartArray = [
+    { id: 1, name: 'Iphone', price: 5000, quantity: 2 },
+    { id: 2, name: 'MacBook Pro', price: 20000, quantity: 1 },
+    { id: 3, name: 'Magic Mouse porcaria', price: 1000, quantity: 5 },
+    { id: 4, name: 'Paninho bosta', price: 200, quantity: 5 },
+]
+
+
+
+// map percore item por item dentro de um array, e com base nisso, cria um novo array
+// sendo possivel criar um novo array com apenas parte do primeiro array, porem mantendo as mesmas 
+// posicoes daqueles indices do array anterior  
+const mapCart = cartArray.map((product) => ({
+        ...product,
+        subtotal: product.quantity * product.price
+}))
+
+
+
+
+console.log({ mapCart })
